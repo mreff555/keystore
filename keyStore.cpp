06197d9e85e158379f2a8fc60a7b9e68135877ce
mreff555 <feerstd@gmail.com>
2017-07-22 17:48:37 -0400

    i forget

--- keyStore.cpp
+++ keyStore.cpp
@@ -3,6 +3,11 @@
 void KeyStore::createKey(string desc, string uin, string pass, string url, bool hidden) {
     KeyStore *k = new KeyStore;
     k->description = desc;
+    k->username = uin;
+    k->password = pass;          // Yes this is temporary.  Password should not be plain text
+    k->address = url;
+    k->hidden = hidden;
+
 
 }
 
